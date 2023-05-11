95838502d6193d5ae3d1c0151e113ccbdfaeab93
Mike Brinker <themikejr@gmail.com>
2023-05-11 16:45:56 -0400

    Index page for posts.

---
+++ content/posts/test.md
@@ -0,0 +1,5 @@
+---
+title: "Test Post"
+description: "This is a test post"
+draft: true
+---
--- pages/posts.vue
+++ pages/posts.vue
@@ -1,13 +1,17 @@
 <script setup>
 const { data } = await useAsyncData("issues", () =>
-  queryContent("issues").find()
+  queryContent("posts").find()
 );
 </script>
 
 <template>
   <main class="container mx-auto prose prose-invert">
-    <h1>Past Issues</h1>
-    <article v-for="issue in data">
+    <h1>Posts</h1>
+    <article v-if="!data.length">
+      <p>There aren't any posts yet.</p>
+    </article>
+
+    <article v-else v-for="issue in data">
       <a :href="issue._path">{{ issue.title }}</a>
       <p class="m-0">{{ issue.description }}</p>
     </article>
