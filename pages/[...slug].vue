939f676568b55897a14d96cf3d5ef94fa96d2554
Mike Brinker <themikejr@gmail.com>
2023-05-11 10:40:26 -0400

    Use nuxt content to display issues.

---
+++ pages/[...slug].vue
@@ -0,0 +1,7 @@
+<template>
+  <main class="container mx-auto prose prose-invert">
+    <article>
+      <ContentDoc />
+    </article>
+  </main>
+</template>
---
+++ pages/issues.vue
@@ -0,0 +1,14 @@
+<script setup>
+const { data } = await useAsyncData('issues', () => queryContent('issues').find());
+</script>
+
+<template>
+  <main class="container mx-auto prose prose-invert">
+    <h1>Past Issues</h1>
+  <article v-for="issue in data"> 
+      <a :href="issue._path">{{ issue.title }}</a>
+      <p class="m-0"> {{ issue.description }}</p>
+    </article>
+  </main>
+</template>
+
