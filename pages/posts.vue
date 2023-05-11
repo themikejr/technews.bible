588d8d2b0e040ffeda032843f41e16ddaf69478e
Mike Brinker <themikejr@gmail.com>
2023-05-11 17:03:05 -0400

    posts index page only shows non-drafts

--- pages/posts.vue
+++ pages/posts.vue
@@ -2,18 +2,20 @@
 const { data } = await useAsyncData("issues", () =>
   queryContent("posts").find()
 );
+
+const filteredData = data.value.filter((post) => post.draft === false);
 </script>
 
 <template>
   <main class="container mx-auto prose prose-invert">
     <h1>Posts</h1>
-    <article v-if="!data.length">
+    <article v-if="!filteredData.length">
       <p>There aren't any posts yet.</p>
     </article>
 
-    <article v-else v-for="issue in data">
-      <a :href="issue._path">{{ issue.title }}</a>
-      <p class="m-0">{{ issue.description }}</p>
+    <article v-else v-for="post in filteredData">
+      <a :href="post._path">{{ post.title }}</a>
+      <p class="m-0">{{ post.description }}</p>
     </article>
   </main>
 </template>
