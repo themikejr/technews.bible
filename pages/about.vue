d698f14a70e9d698af9f00b525767e56c8e60f13
Mike Brinker <themikejr@gmail.com>
2023-05-15 16:32:49 -0400

    update about page, add image.

--- pages/about.vue
+++ pages/about.vue
@@ -2,18 +2,36 @@
   <article class="container mx-auto px-4 prose prose-invert">
     <h1>About</h1>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-      aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-      sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p class="text-xl">
+      In May of 2023, a group of technologists and innovators from several Bible
+      Translation organizations met for a conference. Among the problems
+      discussed were barriers for collaboration between organizations. At one
+      point, the idea for a newsletter that shared news, updates, and case
+      studies with the community came up:
+    </p>
 
-    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
-      aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
-      enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
-    ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
-    adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-    voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-    aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
-    molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+    <figure>
+      <img
+        class="rounded-lg drop-shadow-2xl"
+        src="/img/newsletter-prototype-sketch-web.jpg"
+      />
+      <figcaption>
+        Conclusive evidence that indeed, Mike had an idea.
+      </figcaption>
+    </figure>
 
-</article></template>
+    <p>
+      TechNews.Bible was born. The motivation for TechNews.Bible is to curate,
+      develop, and share content in a way that helps Bible Translation
+      technologists and innovators <b>discover</b>, <b>learn</b>, <b>share</b>,
+      and <b>collaborate</b>.
+    </p>
+
+    <p>
+      This project is intentionally interdependent &mdash; it requires a
+      community to share and contribute content. Content submissoins and other
+      opportunities for collaboration are glady accepted at the
+      <a href="/contact">contact</a> page.
+    </p>
+  </article>
+</template>
