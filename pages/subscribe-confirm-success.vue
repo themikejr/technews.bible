4254939ae04c58c823ba0ad0ab8f47ee45fb9181
Mike Brinker <themikejr@gmail.com>
2023-05-17 10:37:10 -0400

    add subscribe and subscribe-confirm success pages.

---
+++ pages/subscribe-confirm-success.vue
@@ -0,0 +1,16 @@
+<template>
+  <article class="container mx-auto prose prose-invert">
+    <h1>Subscription confirmed.</h1>
+    <p>You're all set. Thank you for your interest!</p>
+
+    <p>
+      You will recieve our next newsletter issue. In the meantime, feel free to
+      browse <NuxtLink to="/issues">Past Issues</NuxtLink>.
+    </p>
+    <p>
+      Whenever you come across a piece of content that might be a good fit for
+      TechNews.Bible, please do not hesitate to share via the
+      <NuxtLink to="/contact">contact</NuxtLink> form.
+    </p>
+  </article>
+</template>
---
+++ pages/subscribe-success.vue
@@ -0,0 +1,15 @@
+<template>
+  <article class="container mx-auto prose prose-invert">
+    <h1>You're subscribed!</h1>
+    <p>
+      Thanks for subscribing to TechNews.Bible. A confirmation email is on it's
+      way. <strong>You must confirm to begin recieving the newsletter</strong>.
+    </p>
+
+    <p>
+      We are always trying to expand our known universe of content and content
+      sources. If you have suggestions, please do not hesitate to share them
+      with us on the <NuxtLink to="/contact">contact</NuxtLink> form.
+    </p>
+  </article>
+</template>
