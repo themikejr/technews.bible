d1239a522ca31d3f95ca8aec51909c97cdef0c84
Mike Brinker <themikejr@gmail.com>
2023-05-19 11:49:52 -0400

    target _blank for external links.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,29 @@ export default defineNuxtConfig({
   content: {
     markdown: {
       anchorLinks: false,
+      rehypePlugins: [
+        [
+          "rehype-external-links",
+          {
+            target: "_blank",
+            // content: {
+            //   type: "element",
+            //   tagName: "icon-external-link",
+            //   properties: {
+            //     className: ["w-4", "h-4", "ml-1", "-mt-1"],
+            //   },
+            // },
+            //   contentProperties: {
+            //     className: [
+            //       "inline-block",
+            //       "align-middle",
+            //       "text-gray-600",
+            //       "dark:text-gray-400",
+            //     ],
+            //   },
+          },
+        ],
+      ],
     },
   },
 
