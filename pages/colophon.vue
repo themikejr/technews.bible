d8398b67b20e20445cc2caaaff5a25ae96bdc26c
Mike Brinker <themikejr@gmail.com>
2023-05-19 12:00:34 -0400

    add links to colophon.

--- pages/colophon.vue
+++ pages/colophon.vue
@@ -6,25 +6,30 @@ useHead({ title: "TechNews.Bible | Colophon" });
   <main class="container mx-auto px-6 prose prose-invert">
     <h1>Colophon</h1>
     <article>
-      <p class="text-4xl text-right mb-0">Tech Stack</p>
+      <p class="text-4xl font-thin text-right mb-0">Tech Stack</p>
 
       <p class="text-white text-2xl leading-relaxed font-thin tracking-wide">
-        <a>Nuxt</a>, the intuitive web framework. <a>Tailwind CSS</a> a
-        utility-first CSS framework. <a>Netlify</a>, the platform developers
-        love.
+        <a href="https://nuxtjs.org" target="_blank">Nuxt</a>, the intuitive web
+        framework.
+        <a href="https://tailwindcss.com" target="_blank">Tailwind CSS</a> a
+        utility-first CSS framework.
+        <a href="https://netlify.com" target="_blank">Netlify</a>, the platform
+        developers love.
       </p>
 
-      <p class="text-4xl text-right mt-[4rem] mb-0">Newsletter</p>
+      <p class="text-4xl font-thin text-right mt-[4rem] mb-0">Newsletter</p>
 
       <p class="text-white text-2xl leading-relaxed font-thin tracking-wide">
-        <a>Buttondown</a>, newsletter software for people like you.
+        <a href="https://buttondown.email" target="_blank">Buttondown</a>,
+        newsletter software for people like you.
       </p>
 
-      <p class="text-4xl text-right mt-[4rem] mb-0">Typeface</p>
+      <p class="text-4xl font-thin text-right mt-[4rem] mb-0">Typeface</p>
 
       <p class="text-white text-2xl leading-relaxed font-thin tracking-wide">
-        <a href="https://www.fontshare.com/fonts/satoshi">Satoshi</a>, a
-        modernist sans serif typeface.
+        <a href="https://www.fontshare.com/fonts/satoshi" target="_blank"
+          >Satoshi</a
+        >, a modernist sans serif typeface.
       </p>
       <!-- <figure class="text-xl"> -->
       <!--   <blockquote class="font-regular not-italic leading-relaxed"> -->
