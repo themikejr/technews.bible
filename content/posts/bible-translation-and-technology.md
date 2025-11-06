85e7bc328e2ddb6a7837902c9f74beadf4d0e6af
Mike Brinker <themikejr@gmail.com>
2025-11-06 08:49:20 -0500

    Seed BT+Technology starter pack

---
+++ content/posts/bible-translation-and-technology.md
@@ -0,0 +1,20 @@
+---
+title: Bible Translation and Technology
+description: Resources for further exploration.
+draft: true
+---
+
+Concept:
+
+newsletters, podcasts, books to read, people to follow, subjects to explore, etc.
+bible data for hobby programming, communities
+
+## List
+
+[ETEN Innovation Lab Substack](https://etenlab.substack.com/)  
+more info about this link...
+
+[Working for the Word: A Bible Translation Podcast](https://workingfortheword.com/)  
+more info here
+
+[Open Components Ecosystem]()
---
+++ pages/bt.vue
@@ -0,0 +1,21 @@
+<script setup>
+const { data: post } = await useAsyncData("bt-post", () =>
+  queryContent("posts/bible-translation-and-technology").findOne()
+);
+
+useHead({
+  meta: [
+    { name: 'robots', content: 'noindex, nofollow' }
+  ]
+});
+</script>
+
+<template>
+  <main class="container mx-auto mb-8 px-6 text-lg prose prose-invert">
+    <article>
+      <h1>{{ post.title }}</h1>
+      <p class="italic">{{ post.description }}</p>
+      <ContentRenderer :value="post" />
+    </article>
+  </main>
+</template>
